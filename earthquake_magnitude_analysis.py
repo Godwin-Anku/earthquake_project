e73fc3eff6ad4fdddf4da642148cd1a7dda6ffac
Godwin-Anku <godwinanku246@gmail.com>
2023-11-18 15:06:59 -0800

    A new feature was added to the application to allow users compute the peak ground acceleration in both meters and micrometers. After inputing the known magnitude of the earthquake, the solve button allows the program to compute the peak amplitude recorded by the Wood-Anderson seismograph in micromters and the Peak ground displacement in both micrometers and meters.

--- earthquake_magnitude_analysis.py
+++ earthquake_magnitude_analysis.py
@@ -11,25 +11,27 @@ class App(tk.Tk):
         self.geometry(f'{size[0]}x{size[1]}')
         self.minsize(size[0], size[1]) 
         self.maxsize(size[0], size[1])   
-
+        
         #create widgets
         self.menu = Menu(self)
         self.mainframe = Mainframe(self)
         #run
         self.mainloop()
 
+
 class Menu(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.place(x = 0, y = 0, relwidth = 1, relheight = 0.5)
         self.create_widgets()
+        self.woodand_mag_amplitude = None
 
     def create_widgets(self):
-        mag_label = ttk.Label(self, text = 'Magnitude')
-        zero_mag_label = ttk.Label(self, text = 'Amplitude of zero magnitude earthquake')
+        mag_label = ttk.Label(self, text = 'Magnitude :')
+        zero_mag_label = ttk.Label(self, text = 'Amplitude of zero magnitude earthquake : 1 micrometer')
 
         #create solve button
-        self.solve_button = ttk.Button(self, text = 'Solve', command = self.calculate_woodand_mag)
+        self.solve_button = ttk.Button(self, text = 'Solve', command = self.solve)
 
         #create entry
         self.mag_entry = ttk.Entry(self)
@@ -48,14 +50,39 @@ class Menu(ttk.Frame):
         #solve button
         self.solve_button.grid(row = 1, column = 1, sticky = 'we')
 
+        
+
+    #Peak amplitude recorded by the wood anderson seismograph
     def calculate_woodand_mag(self):
+        global woodand_mag_amplitude
         try:
             magnitude = float(self.mag_entry.get())
             zero_mag_amplitude = 1.0
             woodand_mag_amplitude = np.power(10, magnitude * zero_mag_amplitude)
-            self.master.mainframe.woodand_mag_label.config(text = f'Maximum amplitude recorded by the Wood-Anderson seismograph: {woodand_mag_amplitude} micrometers')
+            self.master.mainframe.woodand_mag_label.config(text = f'Maximum amplitude recorded by the Wood-Anderson seismograph : {woodand_mag_amplitude:.3f} micrometers / {woodand_mag_amplitude/10**6:.3f} m')
         except ValueError:
             self.master.mainframe.woodand_mag_label.config(text = 'Please enter a valid number')
+        return woodand_mag_amplitude
+
+
+
+    #Peak ground displacement
+    def pgd(self):
+        try:
+            magnification_factor = 2800
+            ground_displacement = (woodand_mag_amplitude/10**6) / magnification_factor
+            self.master.mainframe.ground_displacement_label.config(text = f'Peak ground displacement : {ground_displacement:.3f} meters')
+        except ValueError:
+            self.master.mainframe.ground_displacement_label.config(text = 'Your input is incorrect')
+        return ground_displacement
+    
+
+    #Solve button to calculate the peak ground displacement and peak amplitude recorded  by the wood-anderson seismograph
+    def solve(self):
+        self.calculate_woodand_mag()
+        self.pgd()
+
+
 
 class Mainframe(ttk.Frame):
     def __init__(self, parent):
@@ -63,7 +90,13 @@ class Mainframe(ttk.Frame):
         self.place(x = 0, rely = 0.5, relwidth = 1, relheight = 0.5)
 
         #create widgets
-        self.woodand_mag_label = ttk.Label(self, text = 'Maximum amplitude recorded on a Wood-Anderson seismograph')
+        #wood anderson magnitude label
+        self.woodand_mag_label = ttk.Label(self, text = 'Maximum amplitude recorded on a Wood-Anderson seismograph : 0 meters')
         self.woodand_mag_label.pack(expand = True, fill = 'both')
+
+        #create widgets
+        #ground displacement label
+        self.ground_displacement_label = ttk.Label(self, text = 'Peak ground displacement : 0 meters')
+        self.ground_displacement_label.pack(expand = True, fill = 'both')
 
 App('Earthquake Analysis', (600,400))
